9cbd8067802ed9483701e85ab70b84b835b45937
Ian Ryan <xgix57@gmail.com>
2014-12-27 23:11:38 -0500

    Added config.yml and made different strengths

--- src/co/infnet/hoeguns/HoeGuns.java
+++ src/co/infnet/hoeguns/HoeGuns.java
@@ -3,6 +3,7 @@ package co.infnet.hoeguns;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,6 +12,7 @@ import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -34,8 +36,8 @@ public class HoeGuns extends JavaPlugin implements Listener{
 	@Override
 	public void onEnable() {
 		instance = this;
-		this.saveDefaultConfig();
 		PluginDescriptionFile pdfFile = this.getDescription();
+		this.saveDefaultConfig();
 		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " has been enabled!");
 		getServer().getPluginManager().registerEvents(this, this);
 	}
@@ -47,10 +49,10 @@ public class HoeGuns extends JavaPlugin implements Listener{
 	public void onInteract(PlayerInteractEvent event){ 
 		Player p = event.getPlayer();
 		String uuid = p.getUniqueId().toString();
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR){
-			Snowball snowball = (Snowball) p.launchProjectile(Snowball.class);
-			if(p.getItemInHand().getType() == Material.DIAMOND_HOE || p.getItemInHand().getType() == Material.GOLD_HOE || p.getItemInHand().getType() == Material.IRON_HOE || p.getItemInHand().getType() == Material.STONE_HOE || p.getItemInHand().getType() == Material.WOOD_HOE){
-				if(GunsEnabled.get(uuid)){
+		if(GunsEnabled.get(uuid)){
+			if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR){
+				Snowball snowball = (Snowball) p.launchProjectile(Snowball.class);
+				if(p.getItemInHand().getType() == Material.DIAMOND_HOE || p.getItemInHand().getType() == Material.GOLD_HOE || p.getItemInHand().getType() == Material.IRON_HOE || p.getItemInHand().getType() == Material.STONE_HOE || p.getItemInHand().getType() == Material.WOOD_HOE){
 					snowball.setVelocity(p.getEyeLocation().getDirection().multiply(10D));
 					snowball.setShooter(p);
 				}	
@@ -61,16 +63,40 @@ public class HoeGuns extends JavaPlugin implements Listener{
 	public boolean onCommand(CommandSender s, Command cmd, String c, String args[]){
 		Player p = (Player) s;
 		String uuid = p.getUniqueId().toString();
-		if(c.equalsIgnoreCase("hw")){
+		if(c.equalsIgnoreCase("hg")){
 			if(p.hasPermission("hg.use")){
+				if(GunsEnabled.get(uuid) == null){
+					GunsEnabled.put(uuid, false);
+				}
 				if(!GunsEnabled.get(uuid)){
 					GunsEnabled.put(uuid, true);
-					p.sendMessage("Hoe Guns enabled!");
+					p.sendMessage(ChatColor.GREEN + "Hoe Guns enabled!");
 				}else{
 					GunsEnabled.put(uuid, false);
+					p.sendMessage(ChatColor.RED + "Hoe Guns disabled!");
 				}
 			}	
 		}
 		return true;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onSnowballHit(EntityDamageByEntityEvent e){
+		if(e.getDamager() instanceof Snowball){
+			Snowball snowball = (Snowball) e.getDamager();
+			Player p = (Player) snowball.getShooter();
+			if(p.getItemInHand().getType() == Material.DIAMOND_HOE){
+				e.setDamage(this.getConfig().getInt("DiamondDamage"));
+			}else if(p.getItemInHand().getType() == Material.GOLD_HOE){
+				e.setDamage(this.getConfig().getInt("GoldDamage"));
+			}else if(p.getItemInHand().getType() == Material.IRON_HOE){
+				e.setDamage(this.getConfig().getInt("IronDamage"));
+			}else if(p.getItemInHand().getType() == Material.STONE_HOE){
+				e.setDamage(this.getConfig().getInt("StoneDamage"));
+			}else if(p.getItemInHand().getType() == Material.WOOD_HOE){
+				e.setDamage(this.getConfig().getInt("WoodDamage"));
+			}	
+		}
 	}
 }	
